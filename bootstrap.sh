231c2a567fadb193543e910110abefa8a31ad978
Nathan Dench <ndenc2@gmail.com>
2015-06-07 17:27:21 +1000

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,34 +6,34 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vim vimrc tmux.conf"           # list of files/folders to symlink in homedir
+date=$(date +%Y%m%d_%H%M%S)
+dotfiles=$HOME/dotfiles           # dotfiles directory
+olddir=$HOME/dotfiles_old/$date   # old dotfiles backup directory
+files="vim vimrc tmux.conf"       # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+echo "Creating $olddir for backup of any existing dotfiles"
 mkdir -p $olddir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    if [ -e ~/.$file ]
+    if [ -e $HOME/.$file ]
     then
-        echo "Moving ~/.$file to $olddir"
-        mv ~/.$file $olddir/$file
+        echo "Moving $HOME/.$file to $olddir/$file"
+        mv $HOME/.$file $olddir/$file
     fi
 
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    echo "Creating symlink $HOME/.$file -> $dotfiles/$file"
+    ln -s $dotfiles/$file $HOME/.$file
 done
 
 # Clone in vundle
-VUNDLE_LOCATION="~/.vim/bundle/Vundle.vim"
-if [ ! "$(ls -A $VUNDLE_LOCATION)" ]
+vundle_location="$HOME/.vim/bundle/Vundle.vim"
+if [ ! "$(ls -A $vundle_location)" ]
 then
-    git clone https://github.com/gmarik/Vundle.vim.git $VUNDLE_LOCAITON
+    git clone https://github.com/gmarik/Vundle.vim.git $vundle_location
 fi
 
 # Install bundles
