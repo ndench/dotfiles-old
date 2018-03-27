5ac4b841a2bc33458ada39c1c682eeb6ee2493dd
Nathan Dench <ndenc2@gmail.com>
2018-03-27 16:53:43 +1000

    add done command

--- config-dotfiles/fish/config.fish
+++ config-dotfiles/fish/config.fish
@@ -1,5 +1,4 @@
 alias packer=packer-io
-alias done='pactl set-sink-mute @DEFAULT_SINK@ off; and pactl set-sink-volume @DEFAULT_SINK@ 100%; and espeak done > /dev/null 2>&1'
 
 # Base16 Shell
 if status --is-interactive
